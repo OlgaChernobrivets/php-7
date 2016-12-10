bdf54cc261fa205c043687e9e363b0b718b6a9f8
OlgaChernobrivets <gmco555@gmail.com>
2016-12-10 21:10:17 +0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,16 @@
+<html lang="RU">
+<head>
+  <meta charset="UTF-8">
+<h1>Olga Chernobrivets</h1>
+<?php
+$name='Olga';
+echo $name;
+echo PHP_EOL;
+$age=25;
+echo $age;
+echo PHP_EOL;
+$sity='Omsk';
+echo $sity;
+echo PHP_EOL;
+$inf='Economist';
+echo $inf;
