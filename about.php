384f8314ebb690230ecb040d2ca20d05167712b2
OlgaChernobrivets <gmco555@gmail.com>
2016-12-12 20:35:49 +0500

    Commit after chatting with Alex

--- about.php
+++ about.php
@@ -1,16 +1,54 @@
-<html lang="RU">
-<head>
-  <meta charset="UTF-8">
+Домашнее задание к лекции 1.1 «Введение в PHP»
+
 <h1>Olga Chernobrivets</h1>
 <?php
-$name='Olga';
-echo $name;
-echo PHP_EOL;
+$name='Ольга';
 $age=25;
-echo $age;
-echo PHP_EOL;
-$sity='Omsk';
-echo $sity;
-echo PHP_EOL;
-$inf='Economist';
-echo $inf;
+$sity='Губкинский';
+$email='gmco555@gmail.com';
+$inf='Экономист, котороый влюбился в тестирование';
+?>
+<table>
+	<tr>
+		<td>Имя</td>
+		<td>
+		<?php
+		echo $name;
+		?>
+		</td>
+	</tr>
+	<tr>
+		<td>Возраст</td>
+		<td>
+		<?php
+		echo $age;
+		?>
+		</td>
+	</tr>
+	<tr>
+		<td>Почта</td>
+		<td>
+		<a href="mailto:gmco555@gmail.com">
+			<?php
+		echo $email;
+		?>
+		</a>
+		</td>
+	</tr>
+	<tr>
+		<td>Город</td>
+		<td>
+		<?php
+		echo $sity;
+		?>
+		</td>
+	</tr>
+	<tr>
+		<td>О себе</td>
+		<td>
+		<?php
+		echo $inf;
+		?>
+		</td>
+	</tr>
+</table>
